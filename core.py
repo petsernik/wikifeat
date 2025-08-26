0a6445cc180c4f7f043e38f45e7d4ad6ec20bc14
petsernik <sergei.petrov.comp@gmail.com>
2025-08-26 16:31:58 +0300

    handle http status codes; get image_url in other func; better support for web.archive.org

--- core.py
+++ core.py
@@ -3,6 +3,7 @@ import os
 import re
 import sys
 import requests
+from urllib.parse import urlparse
 import telebot
 from bs4 import BeautifulSoup
 from config import Config
@@ -20,10 +21,68 @@ def get_request(url: str):
     headers = {'User-Agent': 'wikifeat/0.0 (https://github.com/petsernik/wikifeat)'}
     return requests.get(url, headers=headers)
 
+def get_url_by_tag(url, tag):
+    netloc = urlparse(url).netloc
+    if netloc == 'web.archive.org':
+        # Ищем последнюю архивную версию вместо определённой даты, например:
+        # https://web.archive.org/web/20240619223918/... ---> https://web.archive.org/web/2/...
+        return netloc, 'https://' + netloc + '/web/2/' + tag['href'].split('/',3)[3]
+    else:
+        return netloc, 'https://' + netloc + tag['href']
+
+def get_image_parameters(url, img_tag):
+    init = None, [], None
+    if not (img_tag and img_tag.has_attr('src') and img_tag.parent.has_attr('href')):
+        return init
+    image_url, image_licenses, image_page_url = init
+    netloc, image_page_url = get_url_by_tag(url, img_tag.parent)
+    response = get_request(image_page_url)
+    if response.status_code == 404 or response.status_code == 429:
+        return init
+    if response.status_code != 200:
+        raise Exception(f'Unexpected response code when get image page: {response.status_code}\n'
+                        f'Response body: {response.content}')
+
+    image_soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Подбираем разрешение не больше 2500×2500
+    width_max, height_max = 2500, 2500
+    resolutions_span = image_soup.find('span', class_='mw-filepage-other-resolutions')
+    if resolutions_span:
+        links = resolutions_span.find_all('a', href=True)
+        for link in links[::-1]:
+            match = re.search(r'(\d+)\s*[×x]\s*(\d+)', link.text)
+            if match:
+                width, height = int(match.group(1)), int(match.group(2))
+                if width <= width_max and height <= height_max:
+                    image_url = 'https:' + link['href']
+                    break
+    else:
+        # fallback: берём оригинал
+        file_link_tag = image_soup.find('a', class_='internal')
+        if file_link_tag and file_link_tag.has_attr('href'):
+            image_url = 'https:' + file_link_tag['href']
+
+    # Собираем лицензии
+    license_tags = image_soup.find_all(class_=re.compile('licensetpl_short'))
+    for tag in license_tags:
+        clean_text = re.sub(r'\s+', ' ', tag.get_text(strip=True)).strip()
+        image_licenses.append(clean_text)
+
+    image_licenses = list(set(image_licenses))
+    if netloc == 'web.archive.org' and image_url:
+        lst = image_url.split('https://')
+        lst[1] = lst[1][:-1]+'if_/'
+        image_url = 'https://'.join(lst)
+    return image_url, image_licenses, image_page_url
+
 
 def get_featured_article(wiki_url):
     # Загружаем HTML
     response = get_request(wiki_url)
+    if response.status_code != 200:
+        raise Exception(f'Unexpected response code when get wiki page: {response.status_code}\n'
+                        f'Response body: {response.content}')
     response.encoding = 'utf-8'
     soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -31,43 +90,11 @@ def get_featured_article(wiki_url):
     featured_block = soup.find('div', id='main-tfa' if '/Заглавная_страница' in wiki_url else 'mw-content-text')
     paragraphs = [p.get_text().strip() for p in featured_block.find_all('p')]
     link_tag = featured_block.find('a', href=True)
-    title = link_tag['title'] if '/Заглавная_страница' in wiki_url else wiki_url
-    article_link = f"https://ru.wikipedia.org{link_tag['href']}"
+    title = link_tag['title'] if '/Заглавная_страница' in wiki_url else wiki_url.split('/')[-1]
+    _, article_link = get_url_by_tag(wiki_url, link_tag)
     img_tag = featured_block.find('img')
+    image_url, image_licenses, image_page_url = get_image_parameters(wiki_url, img_tag)
 
-    image_url, image_licenses, image_page_url = None, [], None
-
-    # Если нашли картинку → идём на её страницу
-    if img_tag and img_tag.has_attr('src') and img_tag.parent.has_attr('href'):
-        image_page_url = 'https://ru.wikipedia.org' + img_tag.parent['href']
-        image_response = get_request(image_page_url)
-        image_soup = BeautifulSoup(image_response.text, 'html.parser')
-
-        # Подбираем разрешение не больше 2500×2500
-        width_max, height_max = 2500, 2500
-        resolutions_span = image_soup.find('span', class_='mw-filepage-other-resolutions')
-        if resolutions_span:
-            links = resolutions_span.find_all('a', href=True)
-            for link in links[::-1]:
-                match = re.search(r'(\d+)\s*[×x]\s*(\d+)', link.text)
-                if match:
-                    width, height = int(match.group(1)), int(match.group(2))
-                    if width <= width_max and height <= height_max:
-                        image_url = 'https:' + link['href']
-                        break
-        else:
-            # fallback: берём оригинал
-            file_link_tag = image_soup.find('a', class_='internal')
-            if file_link_tag and file_link_tag.has_attr('href'):
-                image_url = 'https:' + file_link_tag['href']
-
-        # Собираем лицензии
-        license_tags = image_soup.find_all(class_=re.compile('licensetpl_short'))
-        for tag in license_tags:
-            clean_text = re.sub(r'\s+', ' ', tag.get_text(strip=True)).strip()
-            image_licenses.append(clean_text)
-
-    image_licenses = list(set(image_licenses))
     return title, paragraphs, image_url, article_link, image_licenses, image_page_url
 
 
