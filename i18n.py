9d9eda6c34cd14263ccacfcbe36cd325e8d97cbb
petsernik <sergei.petrov.comp@gmail.com>
2026-06-02 23:06:00 +0300

    fix: more accurate attribution

--- i18n.py
+++ i18n.py
@@ -538,7 +538,7 @@ def translate(language: str, key: TKey, **kwargs) -> str:
 
 _UNKNOWN_AUTHOR_PATTERN = re.compile(
     r"(неизвест|аноним|не указан|unknown|anonym|unbekannt|inconnu|desconocid|sconosciut|desconhecid|nieznan|невядом|белгісіз"
-    r"|dont know|don't know)",
+    r"|dont know|don't know|not specified)",
     re.IGNORECASE
 )
 
